89715f7bed69f7535bd39c54d439ababdc1eb42a
swappyison <88504971+swappyison@users.noreply.github.com>
2023-08-26 19:15:28 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -15,20 +15,46 @@ def extract_mpd_urls(url):
             # Parse the HTML content
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Find all MPD URLs in the HTML content
-            mpd_urls = re.findall(r'"(https://[^"]+\.mpd)"', str(soup))
-            return mpd_urls
+            # Find the <h1> tag with the show title
+            title_tag = soup.find('h1', {'data-testid': 'titleScreen_descriptionCard_title'})
+
+            if title_tag:
+                # Extract show name, season number, and episode number from the tag's content
+                title_text = title_tag.text
+                match = re.match(r'^(.*?)\s*\|\s*Season\s*(\d+)\s*\|\s*Episode\s*(\d+)', title_text)
+
+                if match:
+                    show_name, season_number, episode_number = match.groups()
+
+                    # Generate the filename
+                    filename = f"{show_name.strip('.')}.S{season_number}.E{episode_number}.mkv"
+
+                    print(f"Extracted {filename} from {url}")
+
+                    # Find all MPD URLs in the HTML content
+                    mpd_urls = re.findall(r'"(https://[^"]+\.mpd)"', str(soup))
+                    return mpd_urls, show_name, season_number, episode_number
+                else:
+                    print(f"Invalid format in the <h1> tag content: {title_text}")
+
+            else:
+                print("No <h1> tag with the show title found.")
+
         else:
             print(f"Failed to retrieve content from {url}. Status code: {response.status_code}")
-            return []
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return []
 
-# Define the function to download an MPD URL using yt-dlp
-def download_mpd_url(mpd_url):
+    return [], "", "", ""  # Return empty values if extraction fails
+
+# Define the function to download an MPD URL using yt-dlp with a custom filename format
+def download_mpd_url(mpd_url, show_name, season_number, episode_number):
     try:
-        # Construct the yt-dlp command
+        # Generate the filename based on the show name, season number, and episode number
+        filename = f"{show_name}.S{season_number}.E{episode_number}.mkv"
+
+        # Construct the yt-dlp command with the custom filename
         cmd = [
             'yt-dlp',
             '--no-part',
@@ -37,14 +63,17 @@ def download_mpd_url(mpd_url):
             '--user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0',
             '--cookies-from-browser', 'firefox',
             '--referer', 'https://www.amazon.in/',
+            '--output', filename,  # Use the custom filename
             mpd_url
         ]
 
         # Run the yt-dlp command to download the MPD URL
         subprocess.run(cmd, check=True)
-        print(f"Downloaded: {mpd_url}")
+        print(f"Downloaded: {filename}")
+
     except subprocess.CalledProcessError as e:
         print(f"Error downloading {mpd_url}: {e}")
+
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
@@ -55,8 +84,9 @@ with open('episode_urls.txt', 'r') as file:
 # Extract and download MPD URLs in parallel
 with ThreadPoolExecutor(max_workers=4) as executor:
     for episode_url in episode_urls:
-        mpd_urls = extract_mpd_urls(episode_url)
+        mpd_urls, show_name, season_number, episode_number = extract_mpd_urls(episode_url)
         if mpd_urls:
-            executor.map(download_mpd_url, mpd_urls)
+            for mpd_url in mpd_urls:
+                download_mpd_url(mpd_url, show_name, season_number, episode_number)
 
 print("All episodes downloaded.")
